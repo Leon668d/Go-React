1e98bc10cd938a7734a8d7ff1365715a59fd3e55
Leon <2856038680@qq,com>
2024-03-01 18:47:00 +0800

    加了jwt

--- api/controllers/login/userController.go
+++ api/controllers/login/userController.go
@@ -2,6 +2,8 @@ package login
 
 import (
 	"errors"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
@@ -10,64 +12,12 @@ import (
 	"main.go/models"
 )
 
-// // 签名密钥
-// const sign_key = "mysecret"
-
-// // 随机字符串
-// var letters = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-// func randStr(str_len int) string {
-// 	rand_bytes := make([]rune, str_len)
-// 	for i := range rand_bytes {
-// 		rand_bytes[i] = letters[rand.Intn(len(letters))]
-// 	}
-// 	return string(rand_bytes)
-// }
-
-// func generateTokenUsingHs256() (string, error) {
-// 	claim := Login{
-// 		ID:       000001,
-// 		Username: "Tom",
-// 		Password: "read_user_info",
-// 		RegisteredClaims: jwt.RegisteredClaims{
-// 			Issuer:    "Auth_Server",                                   // 签发者
-// 			Subject:   "Tom",                                           // 签发对象
-// 			Audience:  jwt.ClaimStrings{"Android_APP", "IOS_APP"},      //签发受众
-// 			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),   //过期时间
-// 			NotBefore: jwt.NewNumericDate(time.Now().Add(time.Second)), //最早使用时间
-// 			IssuedAt:  jwt.NewNumericDate(time.Now()),                  //签发时间
-// 			ID:        randStr(10),                                     // wt ID, 类似于盐值
-// 		},
-// 	}
-// 	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claim).SignedString([]byte(sign_key))
-// 	return token, err
-// }
-
-// func parseTokenHs256(token_string string) (*Login, error) {
-// 	token, err := jwt.ParseWithClaims(token_string, &Login{}, func(token *jwt.Token) (interface{}, error) {
-// 		return []byte(sign_key), nil //返回签名密钥
-// 	})
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	if !token.Valid {
-// 		return nil, errors.New("claim invalid")
-// 	}
-
-// 	claims, ok := token.Claims.(*Login)
-// 	if !ok {
-// 		return nil, errors.New("invalid claim type")
-// 	}
-
-// 	return claims, nil
-// }
+var jwtKey = []byte("your_secret_key")
 
 type Login struct {
-	ID                   uint   `gorm:"primarykey"`
-	Username             string `json:"usernameVal" form:"usernameVal"`
-	Password             string `json:"passwordVal" form:"passwordVal"`
-	jwt.RegisteredClaims        // 标准的 jwt payload。
+	Username string `json:"usernameVal" form:"usernameVal"`
+	Password string `json:"passwordVal" form:"passwordVal"`
+	jwt.StandardClaims
 }
 
 type UserController struct {
@@ -115,7 +65,10 @@ type UserController struct {
 
 func (UserController) LoginCheck(c *gin.Context) {
 	var msg = &Login{}
-	c.ShouldBindJSON(&msg) // 将password 和 username 传给msg
+	if err := c.ShouldBindJSON(&msg); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, "无效的 JSON 格式")
+		return
+	} // 将password 和 username 传给msg
 	// 查看数据库中是否已经有了这个用户名，若有则进入注册页面
 	var tem = &models.User{}
 	err := global.DB.Where("username = ?", msg.Username).First(&tem).Error
@@ -126,19 +79,26 @@ func (UserController) LoginCheck(c *gin.Context) {
 	} else if err != nil {
 		c.String(200, "有其他错误")
 	} else {
-		// 找到记录
-		var tId = tem.ID
-		if msg.Password == tem.Password {
-			c.JSON(200, gin.H{
-				"ID":       tId,
-				"username": tem.Username,
-				"msg":      "success",
-			})
+		if tem.Password == msg.Password {
+			expirationTime := time.Now().Add(5 * time.Minute)
+			claims := &Login{
+				Username: msg.Username,
+				StandardClaims: jwt.StandardClaims{
+					ExpiresAt: expirationTime.Unix(),
+				},
+			}
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			tokenString, err := token.SignedString(jwtKey)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, "生成令牌失败")
+				return
+			}
+
+			c.JSON(http.StatusOK, gin.H{"token": tokenString})
 		} else {
-			c.JSON(200, gin.H{
-				"msg": "密码错误",
-			})
+			c.JSON(http.StatusUnauthorized, "密码错误")
 		}
+
 	}
 
 }
